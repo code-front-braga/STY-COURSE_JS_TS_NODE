a8dd5699842e787aa148542f1a7ac0336cebe8f3
Leonardo Braga <programer_frontbraga@outlook.com>
2024-09-21 10:53:55 -0300

    chore: seção 8 finalizada

--- SECAO_8_EVENTOS/src/index.ts
+++ SECAO_8_EVENTOS/src/index.ts
@@ -1,1 +1,22 @@
+import { EventEmitter } from 'node:events';
 
+const event = new EventEmitter();
+
+const eventHello = (message: string, name: string) => {
+  console.log(message, name);
+};
+
+event.addListener('ola', eventHello);
+event.on('ola', eventHello);
+
+// Recebe o evento apenas uma vez;
+// event.once('ola', eventHello);
+
+setInterval(() => {
+  console.log('Contador', event.listenerCount('ola'));
+
+  event.emit('ola', 'Deu bom!', 'Leo');
+  // event.removeListener('ola', eventHello);
+
+  event.removeAllListeners('ola')
+}, 1000);
