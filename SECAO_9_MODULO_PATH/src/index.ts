db402ebd76abd9d4d1d49fa1964bf8c574f42b75
Leonardo Braga <programer_frontbraga@outlook.com>
2024-09-23 20:59:14 -0300

    seção 9 finalizada

--- SECAO_9_MODULO_PATH/src/index.ts
+++ SECAO_9_MODULO_PATH/src/index.ts
@@ -1,1 +1,19 @@
+import path from 'node:path';
 
+console.log('join:', path.join('/outra-pasta', 'leo'));
+
+console.log('resolve:', path.resolve('src', 'outra-pasta'));
+
+console.log('dirname:', path.dirname('C:/teste/123'));
+
+console.log('dirname/resolve', path.dirname(path.resolve()));
+
+console.log('basename', path.basename('teste'));
+console.log('basename/resolve', path.basename(path.resolve('src')));
+
+/**
+ * path.extname(): essa função é usada para obter a extensão de um caminho de arquivo.
+ * ela retorna parte da string que representa a extensão do arquivo, incluindo o ponto ('.') antes da extensão.
+ */
+
+console.log(path.extname('teste/leo/index.ts'));
